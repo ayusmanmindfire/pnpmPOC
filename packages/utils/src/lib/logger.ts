5c3ae0c8fc36bc27b7f3c86ea7d91235b6b31db4
biswalom22 <Omprakashb@mindfiresolutions.com>
2026-02-10 11:08:18 +0530

    fix: committed removed files

--- packages/utils/src/lib/logger.ts
+++
@@ -1,4 +0,0 @@
-export const logger = (message: string, context?: string) => {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] ${context? `[${context}]` : ''} ${message}`);
-  };
